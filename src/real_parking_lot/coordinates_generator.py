f9429b09417af99f56111650272d33773ca92bbe
ozanulassivaci <ulas.sivaci@ozu.edu.tr>
2026-09-07 00:27:14 +0300

    refactor: reorganize project into src/media/docs layout
    Move the parking-lot motion detector into src/real_parking_lot, drop the
    duplicated 439 MB copy of the training images that had been left inside
    experiments/, and split reference media and exploratory OpenCV scripts
    into their own top-level folders.

---
+++ src/real_parking_lot/coordinates_generator.py
@@ -0,0 +1,90 @@
+import cv2 as open_cv
+import numpy as np
+
+from colors import COLOR_WHITE
+from drawing_utils import draw_contours
+
+
+def _read_image(image_path):
+    # cv2.imread() cannot open files whose absolute path contains non-ASCII
+    # characters on Windows, so images are read as bytes and decoded instead.
+    data = np.fromfile(image_path, dtype=np.uint8)
+    image = open_cv.imdecode(data, open_cv.IMREAD_COLOR)
+    if image is None:
+        raise FileNotFoundError("Could not read image: %s" % image_path)
+    return image
+
+
+class CoordinatesGenerator:
+    KEY_RESET = ord("r")
+    KEY_QUIT = ord("q")
+
+    def __init__(self, image, output, color):
+        self.output = output
+        self.caption = image
+        self.color = color
+
+        self.image = _read_image(image).copy()
+        self.click_count = 0
+        self.ids = 0
+        self.coordinates = []
+
+        open_cv.namedWindow(self.caption, open_cv.WINDOW_GUI_EXPANDED)
+        open_cv.setMouseCallback(self.caption, self.__mouse_callback)
+
+    def generate(self):
+        while True:
+            open_cv.imshow(self.caption, self.image)
+            key = open_cv.waitKey(0)
+
+            if key == CoordinatesGenerator.KEY_RESET:
+                self.image = self.image.copy()
+            elif key == CoordinatesGenerator.KEY_QUIT:
+                break
+        open_cv.destroyWindow(self.caption)
+
+    def __mouse_callback(self, event, x, y, flags, params):
+
+        if event == open_cv.EVENT_LBUTTONDOWN:
+            self.coordinates.append((x, y))
+            self.click_count += 1
+
+            if self.click_count >= 4:
+                self.__handle_done()
+
+            elif self.click_count > 1:
+                self.__handle_click_progress()
+
+        open_cv.imshow(self.caption, self.image)
+
+    def __handle_click_progress(self):
+        open_cv.line(self.image, self.coordinates[-2], self.coordinates[-1], (255, 0, 0), 1)
+
+    def __handle_done(self):
+        open_cv.line(self.image,
+                     self.coordinates[2],
+                     self.coordinates[3],
+                     self.color,
+                     1)
+        open_cv.line(self.image,
+                     self.coordinates[3],
+                     self.coordinates[0],
+                     self.color,
+                     1)
+
+        self.click_count = 0
+
+        coordinates = np.array(self.coordinates)
+
+        self.output.write("-\n          id: " + str(self.ids) + "\n          coordinates: [" +
+                          "[" + str(self.coordinates[0][0]) + "," + str(self.coordinates[0][1]) + "]," +
+                          "[" + str(self.coordinates[1][0]) + "," + str(self.coordinates[1][1]) + "]," +
+                          "[" + str(self.coordinates[2][0]) + "," + str(self.coordinates[2][1]) + "]," +
+                          "[" + str(self.coordinates[3][0]) + "," + str(self.coordinates[3][1]) + "]]\n")
+
+        draw_contours(self.image, coordinates, str(self.ids + 1), COLOR_WHITE)
+
+        for i in range(0, 4):
+            self.coordinates.pop()
+
+        self.ids += 1
